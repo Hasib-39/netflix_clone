71f57434df8d1801df5a1d537be6f6de5210ef07
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-10-29 19:49:24 +0600

    Basic Bottom Navigation Bar added

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,8 +1,24 @@
+import 'dart:async';
+import 'package:netflix_clone/widgets/bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), (){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>const BottomNavBar() ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
