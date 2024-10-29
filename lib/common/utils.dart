1bddeb4a5c4c4b1501213eebfb90edcb864073f4
Hasib Altaf <hasibaltaf@iut-dhaka.edu>
2024-10-29 20:21:41 +0600

    AppBar added on HomeScreen

---
+++ lib/common/utils.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const apiKey = "4ce1092f9d4cb7dbed8c2346d770683c";
+const kBackgroundColor = Colors.black;
+const imageUrl = "https://image.tmdb.org/t/p/w500";
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/common/utils.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,39 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        backgroundColor: kBackgroundColor,
+        title: Image.asset(
+            "assets/logo.png",
+          height: 50,
+          width: 100,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: InkWell(
+              onTap: (){
+
+              },
+                child: const Icon(
+                  size: 30,
+                    Icons.search,
+                  color: Colors.white,
+                ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              height: 27,
+              width: 27,
+            ),
+          ),
+          SizedBox(width: 20,)
+        ],
+      ),
+      body: const Center(
         child: Text('Hi it is Home Screen'),
       ),
     );
